1ec9f43de05c9b3e7042dc9f468f41bdd19f498e
bogdaniy <newbogdaniy@gmail.com>
2016-12-26 22:23:53 +0100

    proccess puason

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
-# T=10
-# intens=0.587
-# t=0;
-# i=1;
-# X=0;
-# Y=0;
-# while t<T
-#     x=rand(1);
-#     dt=-1/intens*log(x)
-#     t=t+dt
-#     X(i)=t;
-#     Y(i)=dt;
-#     i=1+i;
-# end
-# Z=1:(i-1)
-# hist(Y)
-# figure;hist(X)
-# figure;bar(Z,X)
-# X
-# Y
+
 import random
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-T = 10
+T = 50
 intens = 0.587
 t = 0
 X = []
 Y = []
+T_LOG = []
+event_num_probabilities = []
+event_count = 0
+#Run proccess
 while t < T:
     x = random.random()
     dt = - 1 / intens * math.log(x)
     t += dt
     X.append(x)
     Y.append(dt)
+    T_LOG.append(t)
+    event_count += 1
 
-fig, (ax, ax2) = plt.subplots(2)
-ax.bar(np.arange(len(X)), X)
-ax2.bar(np.arange(len(Y)), Y)
+
+for i in range(T):
+    event_num_probability = ((intens * T) ** i * (math.e ** -(intens * T))) / math.factorial(i)
+    event_num_probabilities.append(event_num_probability)
+fig, (ax, ax2, ax3) = plt.subplots(3)
+ax.bar(np.arange(len(event_num_probabilities)), event_num_probabilities, label='probability of occurrence of n events')
+ax.legend(loc='upper left')
+ax2.bar(np.arange(len(Y)), Y, label='Interval between evens')
+ax2.legend(loc='upper left')
+ax3.bar(np.arange(len(T_LOG)), T_LOG, label='time of occurrence given event')
+ax3.legend(loc='upper left')
 plt.show()
